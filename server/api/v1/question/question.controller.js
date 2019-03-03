9102e67a91edc499b372aa0bee3f3fcd520db866
digantaTheProgrammer <digantamandal28@gmail.com>
2019-03-03 11:19:56 +0530

    second commit

--- server/api/v1/question/question.controller.js
+++ server/api/v1/question/question.controller.js
@@ -19,7 +19,7 @@ module.exports.add = function(req, res, next) {
     });
 };
 module.exports.read = function(req, res, next) {
-    Question.find({ category: req.params.qcategory },
+    Question.find({ category: 'finance' },
         "questionText answer category difficultyLevel options questionActive",
         function(err, data) {
             if (err) {
