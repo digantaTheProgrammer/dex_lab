dddd99e266e59adabf38d4e5f5103312ac60501a
digantaTheProgrammer <digantamandal28@gmail.com>
2019-03-03 16:29:12 +0530

    second commit

--- client/angular/controllers/test.controller.js
+++ client/angular/controllers/test.controller.js
@@ -143,6 +143,8 @@ quizApp.controller('test', ['$scope', '$http', '$routeParams', '$window', '$time
         $scope.categoryName = $routeParams.name;
         $http.get('/api/v1/test/show/finance')
             .then(function successCallback(response) {
+                console.log(response.data.data);
+                consoloe.log("OUTCOM");
 
                 main.response = response.data.data;
                 main.seconds = 30;
